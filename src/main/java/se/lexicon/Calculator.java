d89a97e23f36d3ec13273468908c79dee84c59b2
Mohammad Alissa <mohammad.alissa95@hotmail.com>
2022-10-13 15:46:04 +0100

    Hi!

---
+++ src/main/java/se/lexicon/Calculator.java
@@ -0,0 +1,20 @@
+package se.lexicon;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        calc.main();
+
+        Scanner whey = new Scanner(System.in);
+        while(true) {
+            System.out.println("To continue calculating, press Y. Otherwise to stop, press N ");
+            String continueOperation = whey.next();
+            if (continueOperation.equalsIgnoreCase("N")) {
+                System.out.println("Goodbye!");
+                break; }
+            else if (continueOperation.equalsIgnoreCase("Y")) calc.main();
+            else System.out.println("Enter a valid character");
+        }
+    }
+}
